e22465565f8d3ca7ff6a929616a2b1f582b1a173
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-02-23 11:05:30 -0300

    Change server.ts to shared folder

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata';
+import 'express-async-errors';
+import '../../container';
+import express from 'express';
+import connection from '../typeorm';
+import routes from './routes';
+import AppHandleError from '../../errors/AppHandleError';
+
+connection();
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.use(AppHandleError);
+
+app.listen(3333, () => console.log('Server running.'));
